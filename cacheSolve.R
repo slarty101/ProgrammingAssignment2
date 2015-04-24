41f05a80209aa40f913fde2ebca15892555fade7
slarty101 <slarty101@yahoo.co.uk>
2015-04-24 11:08:00 +0100

    Commentary added to cacheSolve function

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,19 @@
+## Function designed to return the inverse of a matrix 
+##
 cacheSolve <- function(x, ...) {
+  
+  ## Check to see if there is data in the cache  
   m <- x$get_inverse()
   if(!is.null(m)) {
     message("Getting cached data...")
     return(m)
   }
   data <- x$get_inverse()
+  
+  ## Create the inverse for the data 
   m <- solve(data, ...)
+  
+  ## Write the inverse data to the cache
   x$set_inverse(m)
   m
 }
