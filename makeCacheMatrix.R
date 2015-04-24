371b0676ae084415687413072328a71fa3fbd7f4
slarty101 <slarty101@yahoo.co.uk>
2015-04-24 11:23:04 +0100

    Commentary added to MakeCacheMatrix function

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,13 +1,24 @@
+## Constructor function to cache a matrix inverse to memory 
+## to speed up processing
 makeCacheMatrix <- function(x = matrix(1:4,2,2)) {
-  m <- NULL
-  set_inverse <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  solve <- function(solve) m <<- solve
-  get_inverse <- function() m
-  list(set_inverse = set_inverse, get_inverse = get_inverse,
-       set_inverse = set_inverse,
-       get_inverse = get_inverse)
+    
+    ## Clear the object first
+    m <- NULL
+    ## Create another object to write the inverse matrix to (i.e.set it)
+    set <- function(y) {
+      ## matrix object
+      x <<- y
+      ## object for the inverse matrix
+      m <<- NULL
+    }
+    get <- function() x
+    ## set the values for the inverse matrix
+    set_inverse <- function(solve) m <<- solve
+    
+    ## retrieve the cached values for the inverse matrix
+    get_inverse <- function() m
+    ## 
+    list(set = set, get = get,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
